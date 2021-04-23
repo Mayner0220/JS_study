a7e37631816f4befbf7745c89c06465cd387b558
Shim Jung Hyun <shj030220@gmail.com>
2021-04-23 10:38:51 +0900

    JS study

---
+++ original_code/practice.js
@@ -0,0 +1,26 @@
+// const arr = [1,2,3,4,5];
+// const [a, b, c, d, e] = arr;
+// console.log(a);
+// console.log(e);
+
+// const obj = {
+//     name: 'seo',
+//     major: 'cs',
+//     age: 29,
+// };
+
+// function call({name, major, age}) {
+//     console.log(name);
+//     console.log(major);
+//     console.log(age);
+// }
+
+// call(obj);
+
+
+const arr = ["a",'b','c','e','d'];
+const obj = {
+    name:
+    age:
+    location:
+}
