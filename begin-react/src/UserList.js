a4ecfc53fddccb9e5f1f3f57b57c34cc7dd8cabc
Shim Jung Hyun <shj030220@gmail.com>
2021-05-14 12:05:33 +0900

    JS study

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+function User({user}) {
+    console.log(user);
+    return (
+        <div>
+            <b>{user.username}</b>
+            <span>({user.email})</span>
+        </div>
+    )
+}
+
 function UserList() {
     const users = [
         {
@@ -20,18 +30,9 @@ function UserList() {
     ];
     return (
         <div>
-            <div>
-                <b>{users[0].username}</b>
-                <span>{"(" + users[0].email + ")"}</span>
-            </div>
-            <div>
-                <b>{users[1].username}</b>
-                <span>{"(" + users[1].email + ")"}</span>
-            </div>
-            <div>
-                <b>{users[2].username}</b>
-                <span>{"(" + users[2].email + ")"}</span>
-            </div>
+            {users.map((user) => (
+                <User user={user} key={user.id}/>
+            ))}
         </div>
     )
 }
