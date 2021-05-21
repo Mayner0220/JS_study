e8bc638fb3f8a3eae5ec6e28d7d89cd579ae5faf
Shim Jung Hyun <shj030220@gmail.com>
2021-05-21 20:53:33 +0900

    JS study

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import UserList from "./UserList";
 
 function App() {
-  return (
-    <UserList />
-  );
+  const users = [
+    {
+      id: 1,
+      username: "name1",
+      email: "abc1@gmail.com",
+    },
+    {
+      id: 2,
+      username: "name2",
+      email: "abc2@gmail.com",
+    },
+    {
+      id: 3,
+      username: "name3",
+      email: "abc3@gmail.com"
+    },
+  ];
+  const nextId = useRef(4);
+  
+  const onCreate = () => {
+    console.log(nextId.current);
+    nextId.current += 1;
+  };
+
+  return <UserList users={users} onCreate={onCreate} />;
 }
 
 export default App;
