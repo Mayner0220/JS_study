2850f82ea281e685734bc25074878538e09f38da
Shim Jung Hyun <shj030220@gmail.com>
2021-05-28 11:27:50 +0900

    JS study

--- begin-react/src/Counter.js
+++ begin-react/src/Counter.js
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
 
+function reducer(state, action) {
+    switch (action.type) {
+        case "INCREMENT":
+            return state + 1;
+        case "DECREMENT":
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
 function Counter() {
     const [number, setNumber] = useState(0);
     const onIncrease = () => {
